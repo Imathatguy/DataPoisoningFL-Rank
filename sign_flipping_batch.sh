6cd433a3b876dc387fe0bd70952e153863d8b358
Imathatguy <ben.zhao@csiro.com.au>
2021-09-06 11:38:46 +1000

    full run sign-flip-mandera

--- sign_flipping_batch.sh
+++ sign_flipping_batch.sh
@@ -2,16 +2,16 @@
 
 # for D in 'FASHION' 'CIFAR10'
 # for M in 'mandera_detect' 'multi_krum' 'bulyan'
-# for R in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+# for R in 0 1 2 3 4 5 6 7 8 9
 # for P in 5 10 15 20 25 30
 
 for D in 'FASHION'
 do
     for M in 'mandera_detect'
     do
-        for R in 0 1 2 3 4
+        for R in 0 1 2 3 4 5 6 7 8 9
         do 
-            for P in 5
+            for P in 5 10 15 20 25 30
             do 
                 sbatch --export D=$D,R=$R,P=$P,M=$M --job-name=sign.$P.$M.$R.$D sign_flipping.jobscript
             done
