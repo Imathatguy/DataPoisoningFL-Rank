5799efafb3845bd225f53a99359493c982ee6515
BenZ-PC\Ben Zhao <ben.zhao159@gmail.com>
2021-11-12 13:50:37 +1100

    fltrust exp run

--- zero_gradient_batch.sh
+++ zero_gradient_batch.sh
@@ -5,9 +5,9 @@
 # for R in 0 1 2 3 4 5 6 7 8 9
 # for P in 5 10 15 20 25 30
 
-for D in 'MNIST'
+for D in 'FASHION' 'CIFAR10'
 do
-    for M in 'mandera_detect' 'multi_krum' 'bulyan' 'median' 'tr_mean' 'fltrust'
+    for M in 'fltrust'
     do
         for R in 0 1 2 3 4 5 6 7 8 9
         do 
