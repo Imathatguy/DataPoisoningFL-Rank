debe5f3bed020945fdecbed605d18e8e004f2a48
Ben Zhao <ben_zi.zhao@mq.edu.au>
2022-05-06 23:11:19 +1000

    tensor std fix + windows .bat for running

--- shifted_mean_bash.bat
+++ shifted_mean_bash.bat
@@ -2,9 +2,9 @@
 SetLocal
 
 for %%D in (FASHION) do (
-    for %%M in (fltrust) do (
-        for %%R in (0) do (
-            for %%P in (5) do (
+    for %%M in (mandera_detect multi_krum bulyan median tr_mean fltrust) do (
+        for %%R in (0 1 2 3 4 5 6 7 8 9) do (
+            for %%P in (5 10 15 20 25 30) do (
                 python shifted_mean_attack.py --dataset %%D --p_workers %%P --def_method %%M --rep_n %%R
             )
         )
