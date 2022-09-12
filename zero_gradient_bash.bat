cd2c469861b137e4e1f499c06031c8c749ff382c
Ben Zhao <ben_zi.zhao@mq.edu.au>
2022-09-12 15:07:57 +1000

    windows bat files for running scripts

--- zero_gradient_bash.bat
+++ zero_gradient_bash.bat
@@ -5,7 +5,7 @@ for %%D in (QMNIST) do (
     for %%M in (None) do (
         for %%R in (1) do (
             for %%P in (30) do (
-                python shifted_mean_attack.py --dataset %%D --p_workers %%P --def_method %%M --rep_n %%R
+                python zero_gradient_attack.py --dataset %%D --p_workers %%P --def_method %%M --rep_n %%R
             )
         )
     )
